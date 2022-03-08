7d166a0e28bd99a33c5debec49f11c1e72d2a70b
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-03-07 22:43:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var windowWidth = window.innerWidth;
+const img = document.querySelector('.img-curva')
+if(windowWidth <=975){
+    img.setAttribute('src', '/fylo-landing-page-with-two-column-layout-master/fylo-landing-page-with-two-column-layout-master/images/bg-curve-mobile.svg')
+    console.log(windowWidth)
+}
